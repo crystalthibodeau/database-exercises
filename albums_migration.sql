5651de65e9ad1138ef8ea8c6a9dfd18809797c54
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-02-19 16:46:10 -0600

    adding albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,8 +1,7 @@
-
 CREATE DATABASE IF NOT EXISTS codeup_test_db;
 USE codeup_test_db;
 CREATE TABLE IF NOT EXISTS albums (
-id int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 artist char(50) NOT NULL,
 name varchar(100) NOT NULL,
 release_date int(4) NOT NULL,
--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,11 +1,6 @@
 USE codeup_test_db;
 CREATE DATABASE IF NOT EXISTS codeup_test_db;
 
--- Use INSERT to add records for all the albums from this list on Wikipedia that claim over 30 million sales (the first two tables). For sales data, use the 'sales certification' column of the tables, not 'claimed sales'. For artists listed with 'Various Artists', just use the primary artist's name.
---
--- First write your queries as separate INSERT statements for each record and test. You should see no output.
--- Refactor your script to use a single INSERT statement for all the records and test it again. Again, this should not generate any output.
--- Note that running the albums_seeder.sql multiple times will generate duplicate data, don't worry about that for now (we'll fix it by the end of the exercise).
 INSERT into albums (artist, name, release_date, sales, genre)
 values ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop, post-disco, funk and rock'),
        ('AC/DC', 'Back in Black', 1980, 29.4, 'Hard rock'),
